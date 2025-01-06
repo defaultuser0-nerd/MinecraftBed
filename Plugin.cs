171bf16654b30ed4a948fa414a6ec3284c6369d5
defaultuser0 <axolotlslmao@gmail.com>
2025-01-06 10:04:33 -0600

    formatting fix

--- Plugin.cs
+++ Plugin.cs
@@ -10,28 +10,28 @@ namespace AssetBundleTemplate
 	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 	public class Plugin : BaseUnityPlugin
 	{
-		public static Plugin instance;
-        public static AssetBundle bundle;
-        public static GameObject assetBundleParent;
-        public static string parentName = "bed";
-        void Start()
-		{
-            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
-		}
-		void OnGameInitialized()
-		{
-			instance = this;
-            bundle = LoadAssetBundle("MinecraftBed.bed");
-            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
-            assetBundleParent.transform.position = new Vector3(-67.2225f, 11.575f, -82.6090f);
-            Destroy(GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/StrawBed_Stump (combined by EdMeshCombiner)/").gameObject);
-        }
-        public AssetBundle LoadAssetBundle(string path)
-        {
-            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
-            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
-            stream.Close();
-            return bundle;
-        }
-    }
+	   public static Plugin instance;
+           public static AssetBundle bundle;
+           public static GameObject assetBundleParent;
+           public static string parentName = "bed";
+           void Start()
+	   {
+	   	GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+	   }
+	   void OnGameInitialized()
+	   {
+	   	instance = this;
+	   	bundle = LoadAssetBundle("MinecraftBed.bed");
+	   	assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
+	   	assetBundleParent.transform.position = new Vector3(-67.2225f, 11.575f, -82.6090f);
+	   	Destroy(GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/StrawBed_Stump (combined by EdMeshCombiner)/").gameObject);
+	   }
+	   public AssetBundle LoadAssetBundle(string path)
+	   {
+	   	Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+	   	AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+	   	stream.Close();
+	   	return bundle;
+	   }
+      }
 }
