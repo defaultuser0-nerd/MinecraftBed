896859a0b7af41a6c5900c77591a188888f1c508
defaultuser0 <axolotlslmao@gmail.com>
2025-01-06 09:43:19 -0600

    yhiunjkrdfslogvybc7 iv896hjiondrgdb6c7u9v8thjieidrf v67gtcguhdscc7f6t7v5x8ysygwshd6xghuj

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace AssetBundleTemplate
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public static Plugin instance;
+        public static AssetBundle bundle;
+        public static GameObject assetBundleParent;
+        public static string parentName = "bed";
+        void Start()
+		{
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+		}
+		void OnGameInitialized()
+		{
+			instance = this;
+            bundle = LoadAssetBundle("MinecraftBed.bed");
+            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
+            assetBundleParent.transform.position = new Vector3(-67.2225f, 11.575f, -82.6090f);
+            Destroy(GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/StrawBed_Stump (combined by EdMeshCombiner)/").gameObject);
+        }
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace AssetBundleTemplate
+{
+    class PluginInfo
+    {
+        public const string GUID = "defaultuser0.MinecraftBed";
+        public const string Name = "MinecraftBed";
+        public const string Version = "1.0.0";
+    }
+}
