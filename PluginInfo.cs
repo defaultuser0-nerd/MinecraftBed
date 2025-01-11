47673387e29eb73b58cb33c785b39cc54c7015e3
defaultuser0 <axolotlslmao@gmail.com>
2025-01-11 11:22:42 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "defaultuser0.MinecraftBed";
         public const string Name = "MinecraftBed";
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
     }
 }
